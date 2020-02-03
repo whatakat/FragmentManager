9ba1b49415041094d692e0cd609b3abbf581d73f
Leoneed <40763564+whatakat@users.noreply.github.com>
2020-02-03 21:06:23 +0300

    add ListenerOnRemove

--- app/src/main/java/com/bankmtk/fragmentmanager/MainActivity.java
+++ app/src/main/java/com/bankmtk/fragmentmanager/MainActivity.java
@@ -1,8 +1,11 @@
 package com.bankmtk.fragmentmanager;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -36,7 +39,39 @@ public class MainActivity extends AppCompatActivity {
         replace2.setOnClickListener(new ListenerOnReplace(fragment2));
         Button replace3 = (Button)findViewById(R.id.replace3);
         replace3.setOnClickListener(new ListenerOnReplace(fragment3));
+    }
+    class ListenerOnAdd implements View.OnClickListener{
+        Fragment fragment;
+        ListenerOnAdd(Fragment fragment){
+            this.fragment = fragment;
+        }
 
+        @Override
+        public void onClick(View v) {
+            addFragment();
+        }
+        private void addFragment(){
+            FragmentTransaction fragmentTransaction =
+                    getSupportFragmentManager().beginTransaction();
+            fragmentTransaction.add(R.id.fragment_container, fragment);
+            fragmentTransaction.commit();
+        }
+    }
+    class ListenerOnRemove implements View.OnClickListener{
+        Fragment fragment;
+        ListenerOnRemove(Fragment fragment){
+            this.fragment = fragment;
+        }
 
+        @Override
+        public void onClick(View v) {
+            removeFragment();
+        }
+        private void removeFragment(){
+            FragmentTransaction fragmentTransaction =
+                    getSupportFragmentManager().beginTransaction();
+            fragmentTransaction.remove(fragment);
+            fragmentTransaction.commit();
+        }
     }
 }
